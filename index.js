8ad22f0dbc5317dba50e547817f9808929202ba8
Arusekk <arek_koz@o2.pl>
2021-01-08 21:32:04 +0100

    Pug.js

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.use(express.static('./static'));
 
+const rooms = {};
+
 app.get('/', (req, res) => {
-  res.end('works');
+  res.render('index', { rooms });
+});
+
+io.on('connection', socket => {
+  console.log('client connected: ', socket.id);
+  socket.on('room', data => {
+    rooms[socket.id] = data;
+  });
+  socket.on('sdp', data => {
+    socket.broadcast.emit('sdp', data);
+  });
+  socket.on('candidate', data => {
+    socket.broadcast.emit('candidate', data);
+  });
 });
 
 server.listen(process.env.PORT || 8000);
